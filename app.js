834fef7985583b850897fa31262d14006cee160c
AnsariAbdullah <ansariabdullahar@gmail.com>
2020-03-03 23:53:54 +0530

    add play pause functionality

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const app = () => {
 	const song = document.querySelector('.song');
-	const play = document.querySelector('play');
+	const play = document.querySelector('.play');
 	const outline = document.querySelector('.moving-outline circle');
 	const video = document.querySelector('.vid-container video');
 	
@@ -16,6 +16,26 @@ const app = () => {
 
 	// Duration
 	let fakeDuration = 600;
+	outline.style.strokeDasharray = outlineLength;
+	outline.style.strokeDashoffset = outlineLength;
+
+	// play sounds
+	play.addEventListener('click', ()=>{
+		checkPlaying(song);
+	})
+
+	// function to play and pause sound
+	const checkPlaying = song => {
+		if(song.paused){
+			song.play();
+			video.play();
+			play.src = './svg/pause.svg';
+		}else{
+			song.pause();
+			video.pause();
+			play.src = './svg/play.svg';
+		}
+	}
 }
 
-app()
+app();
